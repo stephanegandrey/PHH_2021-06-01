c0f41942c618dd33bfd2825af7fdae2c7877b86c
stephanegandrey <st.gandrey@gmail.com>
2021-06-01 17:32:34 +0200

    avec recup variable

--- traitement-formulaire.php
+++ traitement-formulaire.php
@@ -12,6 +12,38 @@
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 <body>
-    
+    <?php
+        //declaration variable
+        $mavar=0;
+        //affectation d'une variable
+        $mavar=$mavar+3;
+        //affichage variable
+        echo $mavar;
+    ?>
+
+    <?php
+        $mesdata=null;
+        if (count($_POST)>0){
+            //echo "j'ai sélectionné"
+            $mesdata=$_POST;
+        }elseif (count($_GET)>0){
+            $mesdata=$_GET;
+        }else{
+            echo "rien à voir ici";
+            exit(0);
+        }
+    ?>
+
+    <h1>tableau $mesdata</h1>
+    <?php 
+        //affichage variable complexe
+        print_r($mesdata) ; 
+    ?>
+    <h1>position 1 $mesdata</h1>
+    <?php
+        //affichaga d'un champ du tablea
+        echo $mesdata["description-produit"];
+    ?>
+
 </body>
 </html>
